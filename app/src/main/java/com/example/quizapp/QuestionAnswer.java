5cea6c3c9ddb458dabe6b524bda860d1de88e290
waykuleshri@gmail.com <waykuleshri@gmail.com>
2022-09-15 22:54:14 +0530

    Sample Commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quizapp;
+
+class QuestionAnswer {
+
+    public static String[] question ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Who is known as father of java?",
+            "Which company owns the Apple?"
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"James Gosling","Guido van Rossum","Bjarne Stroustrup","Tim Berners-Lee"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Notepad",
+            "James Gosling",
+            "Apple"
+    };
+
+}
